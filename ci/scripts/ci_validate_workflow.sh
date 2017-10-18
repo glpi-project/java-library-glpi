20bca06af2cd87a4fbc6f6a9dce4f4318471580b
Rafa Hernandez <rhernandez@teclib.com>
2017-10-18 12:00:05 -0400

    ci(circleci): setup headers, variables and paths

--- ci/scripts/ci_validate_workflow.sh
+++ ci/scripts/ci_validate_workflow.sh
@@ -1,4 +1,29 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#
+#  Copyright (C) 2017 Teclib'
+#
+#  This file is part of Flyve MDM android-library-glpi
+#
+#  Flyve MDM android-library-glpi is a subproject of Flyve MDM. Flyve MDM is a mobile
+#  device management software.
+#
+#  Flyve MDM Android is free software: you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 3
+#  of the License, or (at your option) any later version.
+#
+#  Flyve MDM android-library-glpi is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  ------------------------------------------------------------------------------
+#  @author    Rafael Hernandez - rafaelje
+#  @copyright Copyright (c) 2017 Flyve MDM
+#  @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
+#  @link      https://github.com/flyve-mdm/android-library-glpi
+#  @link      http://www.glpi-project.org/
+#  @link      https://flyve-mdm.com/
+#  ------------------------------------------------------------------------------
 
 GH_COMMIT_MESSAGE=$(git log --pretty=oneline -n 1 $CIRCLE_SHA1)
 
