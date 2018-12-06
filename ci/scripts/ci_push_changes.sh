405ba4374763cf186c81ebdd40c9ca18b6f73124
Ivan Del Pino <idelpino@teclib.com>
2018-12-06 12:05:58 -0400

    fix(config): changes in config and scripts files
    Signed-off-by: Ivan Del Pino <idelpino@teclib.com>

--- ci/scripts/ci_push_changes.sh
+++ ci/scripts/ci_push_changes.sh
@@ -25,17 +25,20 @@
 
 # Push commits and tags to origin branch
 git push --follow-tags origin $CIRCLE_BRANCH
-
-# Merge back the develop branch step
-
-# go to develop
-git checkout develop
-
-# merge with master
-git rebase master
-
-# push develop
-git push origin develop
-
-# return to master
-git checkout master
+#
+## Merge back the develop branch step
+#
+## go to develop
+#git checkout develop
+#git add .
+#git stash
+#
+## merge with master
+#git rebase master
+#
+#
+## push develop
+#git push origin develop
+#
+## return to master
+#git checkout master
